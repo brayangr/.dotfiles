418dbf0e32eeb1ee64023561ed59bc85c6c34d79
Brayan Gonzalez <brayangonzalez@MacBook-Pro-de-Brayan.local>
2023-09-05 19:40:32 -0300

    add neovim settings

---
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+     dependencies = { 'nvim-lua/plenary.nvim' },
+     keys = {
+	{ '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = '(TS) Find files' } },
+	{ '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = '(TS) Find buffers'} }
+     }
+}
