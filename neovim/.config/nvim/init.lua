3653ba46e6c97af5fae2d7f02a279fc7b9eb2c29
Brayan Gonzalez <brayangonzalez@MacBook-Pro-de-Brayan.local>
2023-09-07 01:09:38 -0300

    update neovim config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
+vim.opt.colorcolumn = { 80, 100 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
      dependencies = { 'nvim-lua/plenary.nvim' },
      keys = {
 	{ '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = '(TS) Find files' } },
-	{ '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = '(TS) Find buffers'} }
+	{ '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = '(TS) Find buffers'} },
+	{ '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = '(TS) Fuzzy finder'} }
      }
 }
