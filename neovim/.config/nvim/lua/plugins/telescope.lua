65ed10addac272d84c957f88910928f4a83d132a
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2024-01-06 18:21:15 -0300

    update telescope keybinds

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
      dependencies = { 'nvim-lua/plenary.nvim' },
      keys = {
-	{ '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = '(TS) Find files' } },
-	{ '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = '(TS) Find buffers'} },
-	{ '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = '(TS) Fuzzy finder'} }
+	{ '<leader>sg', function() require('telescope.builtin').git_files() end, { desc = '(TS) Find  Git files' } },
+	{ '<leader>sf', function() require('telescope.builtin').find_files() end, { desc = '(TS) Find files' } },
+	{ '<leader>sb', function() require('telescope.builtin').buffers() end, { desc = '(TS) Find buffers'} },
+	{ '<leader>slg', function() require('telescope.builtin').live_grep() end, { desc = '(TS) Fuzzy finder'} }
      }
 }
