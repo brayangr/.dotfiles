c7afe7efe768cb59da759babd36a12fa16157841
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2024-01-10 17:04:24 -0300

    gitignore

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
 vim.opt.colorcolumn = { 80, 100 }
+vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
